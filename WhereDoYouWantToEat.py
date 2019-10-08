46759b3c04248730083333bdd0310318f08e5e34
Justin Wu <56046340+JWuWuTang@users.noreply.github.com>
2019-10-08 14:35:06 -0700

    Restaurant Cuisines
    Look into using Yelp API to output something clean vs. putting categories within the dictionary. 
    print(businesses[0]["categories"]) outputs the categories and alias, but doesn't look clean

--- WhereDoYouWantToEat.py
+++ WhereDoYouWantToEat.py
@@ -21,23 +21,27 @@ while  meal.upper() != 'Q':
     if (upperMeal == "QUIT" or upperMeal == "Q"):
         break
     location = input("Please input what city you are in:")
-    lunchDict = {"Uroko Cafe", "The Marketplace", "Aloha Hawaiian BBQ", "University Town Center"} #Lunch places
-    dinnerDict = {"Cava Grill", "Panini Kebab Grill", "Souplantation"} #Dinner places
-    bobaDict = {"7 Leaves", "Tastea", "Omomo", "HNTea"} #Boba places
-    dessertDict = {"Yogurtland", "Saffron and Rose", "Fill Bakeshop and Creamery" }
+    lunchDict = {"Uroko Cafe":"Poke", "The Marketplace":"Various", "Aloha Hawaiian BBQ":"Hawaiian", "University Town Center":"Various"} #Lunch places
+    dinnerDict = {"Cava Grill":"Mediterranean", "Panini Kebab Grill":"Mediterranean", "Souplantation":"American"} #Dinner places
+    bobaDict = {"7 Leaves":"Coffee and Tea", "Tastea":"Smoothies and Tea", "Omomo":"Tea", "HNTea":"Tea"} #Boba places
+    dessertDict = {"Yogurtland":"Frozen Yogurt", "Saffron and Rose":"Ice Cream", "Fill Bakeshop and Creamery":"Mochi Donuts"}
 
     if (upperMeal == "LUNCH" or upperMeal == "L"):
         lunch = (random.choice(list(lunchDict))) #Randomly selects from the lunchDict
         params = {'term':lunch,'location':location} #Sets parameters to randomly selected business and searches based off location
+        cuisine = lunchDict.pop(lunch)
     elif (upperMeal == "DINNER" or upperMeal == "D"):
         dinner = (random.choice(list(dinnerDict))) #Randomly selects from the dinnerDict
         params = {'term':dinner,'location':location}
+        cuisine = dinnerDict.pop(dinner)
     elif (upperMeal == "BOBA" or upperMeal == "B"):
         boba = (random.choice(list(bobaDict))) #Randomly selects from the bobaDict
         params = {'term':boba,'location':location}
+        cuisine = bobaDict.pop(boba)
     elif (upperMeal == "DESSERT" or upperMeal == "E"):
         dessert = (random.choice(list(dessertDict))) #Randomly selects from the dessertDict
         params = {'term':dessert,'location':location}
+        cuisine = dessertDict.pop(dessert)
     else:
         print("Please Enter a Valid Option")
         break
@@ -46,6 +50,7 @@ while  meal.upper() != 'Q':
     parsed = json.loads(req.text)
     businesses = parsed["businesses"]
     print(businesses[0]["name"]) #Name of first business on Yelp search
+    print(cuisine)
     print(businesses[0]["url"]) #URL of first business on Yelp search
 
 
